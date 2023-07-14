0cdae10ee555cf31c9a2670f8933fb607394dfdd
Sharvesh83 <sharvesh.somasundaram@kalvium.community>
2023-07-14 22:54:53 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,83 +1,83 @@
-
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import './App.css';
 
 // Do not change this
 const LARGE_NUMBER = 1000000000;
 
 function App() {
-
-
   const [value, setValue] = useState(0);
   const [dark, setTheme] = useState(true);
-  const [themeName, setThemeName] = useState("dark");
+  const [themeName, setThemeName] = useState('dark');
   const [currentList, setList] = useState([]);
-
-
-  // should not change the LOGIC inside this function - you can make changes to the function but logic should NOT change
-  const delayFunction = ()=> {
-    console.log("Delay Function Ran")
-    for(let index=0; index<LARGE_NUMBER; index++){};
-    return value+2;
-  
-  }
+  const [delayResult, setDelayResult] = useState(null);
+  const delayFunctionRef = useRef();
 
   // should not change the LOGIC inside this function - you can make changes to the function but logic should NOT change
-  const testFunction = ()=>{
-    return [value*3 ,value*4]
-  }
+  const delayFunction = useCallback(() => {
+    console.log('Delay Function Ran');
+    for (let index = 0; index < LARGE_NUMBER; index++) {}
+    return value + 2;
+  }, [value]);
 
-  
+  // Assign the delayFunction to delayFunctionRef.current
+  useEffect(() => {
+    delayFunctionRef.current = delayFunction;
+  }, [delayFunction]);
 
-  // should not change this
-  useEffect(()=>{
-    console.log("Callback Function was called")
-  },[testFunction])
-
-
-
-  useEffect(()=>{
-    if(dark){
-      setThemeName("dark")
+  // Update themeName based on dark state
+  useEffect(() => {
+    if (dark) {
+      setThemeName('dark');
+    } else {
+      setThemeName('light');
     }
-    else{
-      setThemeName("light")
+  }, [dark]);
+
+  // Log the callback function when value and delayFunctionRef change
+  useEffect(() => {
+    if (delayFunctionRef.current && value !== 0) {
+      console.log('Callback Function was called');
     }
-  },[dark])
+  }, [value, delayFunctionRef]);
 
-
-  const handleClick = ()=>{
+  // Toggle dark theme
+  const handleClick = () => {
     setTheme(!dark);
-  }
+  };
 
-  const handleChangeValue = ()=>{
-    setValue(value+1);
-  }
+  // Change the value and update the delay result
+  const handleChangeValue = () => {
+    setDelayResult(delayFunctionRef.current());
+    setValue(value + 1);
+  };
 
-  const handleList = ()=>{
-    setList(testFunction);
-  }
+  // Update the list using the testFunction
+  const handleList = () => {
+    setList(testFunction());
+  };
+
+  // should not change the LOGIC inside this function - you can make changes to the function but logic should NOT change
+  const testFunction = useCallback(() => {
+    return [value * 3, value * 4];
+  }, [value]);
 
   const styleTheme = {
-   backgroundColor:dark ? "black":"#ccc7c7",
-  }
+    backgroundColor: dark ? 'black' : '#ccc7c7',
+  };
 
   return (
-    
     <div className="page" style={styleTheme}>
       <button onClick={handleClick}>{themeName}</button>
-      <h1 >{value}</h1>
+      <h1>{value}</h1>
       <button onClick={handleChangeValue}>Change Value</button>
       <button onClick={handleList}>Show List</button>
-      <h2>{delayFunction()}</h2>
+      <h2>{delayResult}</h2>
       <div>
-        {currentList.map((item,index)=>{
-          return <h2 key={index}>{item}</h2>
+        {currentList.map((item, index) => {
+          return <h2 key={index}>{item}</h2>;
         })}
-
       </div>
     </div>
-    
   );
 }
 
